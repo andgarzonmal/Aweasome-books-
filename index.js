052dfe600ed78fbedd77dd57471301ea64df7193
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-04-19 08:24:10 -0500

    Add function add and remove

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function show(books) {
 }
 
 show(books);
+
 if (books !== '') {
   const removeBtn = document.querySelectorAll('.remove-btn');
   removeBtn.forEach((element) => {
@@ -32,32 +33,39 @@ if (books !== '') {
   });
 }
 
+function add(){
+  const newtitle = title.value;
+  const newauthor = author.value;
+  books.push({ name: newtitle, owner: newauthor });
+  shelfbook.innerHTML += `
+  <div class="${newtitle}">
+    <h2>${newtitle}</h2>
+    <p>${newauthor}</p>
+    <button class="remove-btn">
+      Remove
+    </button>
+    <hr>
+  </div>`;
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function remove(parent){
+  parent.remove();
+        books = books.filter((x) => x.name !== parent.className);
+        localStorage.setItem('books', JSON.stringify(books));
+}
 addbtn.addEventListener('click', () => {
   if (title.value === '' || author.value === '') {
     msga.classList.add('active');
   } else if (books.filter((element) => element.name === title.value).length !== 0) { msgb.classList.add('active'); } else {
     msga.classList.remove('active');
     msgb.classList.remove('active');
-    const newtitle = title.value;
-    const newauthor = author.value;
-    books.push({ name: newtitle, owner: newauthor });
-    shelfbook.innerHTML += `
-    <div class="${newtitle}">
-      <h2>${newtitle}</h2>
-      <p>${newauthor}</p>
-      <button class="remove-btn">
-        Remove
-      </button>
-      <hr>
-    </div>`;
-    localStorage.setItem('books', JSON.stringify(books));
+    add();
     const removeBtn = document.querySelectorAll('.remove-btn');
     removeBtn.forEach((element) => {
       element.addEventListener('click', () => {
         const parent = element.parentNode;
-        parent.remove();
-        books = books.filter((x) => x.name !== parent.className);
-        localStorage.setItem('books', JSON.stringify(books));
+        remove(parent)
       });
     });
   }
