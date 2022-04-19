ffc86b5a8a83d0fd2e7cecfca0ea2a854d60b0f2
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-04-19 14:37:01 -0500

    Add remove method to class Shelfbook

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-class  Bookshelf {
-  constructor (){
-    this.book=[]
+class Bookshelf {
+  constructor() {
+    this.book = [];
   }
 
-  add(title, author, books, shelfbook) {
+  add(title, author, shelfbook) {
     const newtitle = title;
     const newauthor = author;
-    
-    books.push({ name: newtitle, owner: newauthor });
+    this.book.push({ name: newtitle, owner: newauthor });
     shelfbook.innerHTML += `
     <div class="${newtitle}">
       <h2>${newtitle}</h2>
@@ -17,14 +16,17 @@ class  Bookshelf {
       </button>
       <hr>
     </div>`;
-    localStorage.setItem('books', JSON.stringify(books));
-  }  
+    localStorage.setItem('books', JSON.stringify(this.book));
+  }
+
+  remove(parent) {
+    parent.remove();
+    this.book = this.book.filter((x) => x.name !== parent.className);
+    localStorage.setItem('books', JSON.stringify(this.book));
+  }
 }
-//#########
-let books = new Bookshelf();
 
-
-console.log(books);
+const books = new Bookshelf();
 const bookstorage = JSON.parse(localStorage.getItem('books'));
 if (bookstorage != null) {
   books.book = bookstorage;
@@ -35,6 +37,7 @@ const shelfbook = document.querySelector('.book-shelf');
 const addbtn = document.querySelector('.add-btn');
 const msga = document.querySelector('.msga');
 const msgb = document.querySelector('.msgb');
+
 if (books.book != null) {
   books.book.forEach((element) => {
     shelfbook.innerHTML += `
@@ -61,44 +64,19 @@ if (books.book !== '') {
   });
 }
 
-// function add() {
-//   const newtitle = title.value;
-//   const newauthor = author.value;
-//   books.push({ name: newtitle, owner: newauthor });
-//   shelfbook.innerHTML += `
-//   <div class="${newtitle}">
-//     <h2>${newtitle}</h2>
-//     <p>${newauthor}</p>
-//     <button class="remove-btn">
-//       Remove
-//     </button>
-//     <hr>
-//   </div>`;
-//  
-// }
-
-function remove(parent) {
-  parent.remove();
-  books = books.filter((x) => x.name !== parent.className);
-  localStorage.setItem('books', JSON.stringify(books));
-}
 addbtn.addEventListener('click', () => {
   if (title.value === '' || author.value === '') {
     msga.classList.add('active');
   } else if (books.book.filter((element) => element.name === title.value).length !== 0) { msgb.classList.add('active'); } else {
     msga.classList.remove('active');
     msgb.classList.remove('active');
-    books.add(title.value, author.value, books.book, shelfbook);
+    books.add(title.value, author.value, shelfbook);
     const removeBtn = document.querySelectorAll('.remove-btn');
     removeBtn.forEach((element) => {
       element.addEventListener('click', () => {
         const parent = element.parentNode;
-        remove(parent);
+        books.remove(parent);
       });
     });
   }
 });
-
-
-
-
