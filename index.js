84e9bb0829bd78aa184d945882015ad982390a98
andgarzonmal <andgarzonmal@unal.edu.co>
2022-04-20 11:36:49 -0500

    ➕ ADD changing colors on nav when selected

--- index.js
+++ index.js
@@ -67,17 +67,25 @@ navitems.forEach((element,index) => {element.addEventListener("click",()=>{
     shelfbook.classList.remove("active")
     contact.classList.remove("active")
     userinput.classList.remove("active")
-    
+    nav1.style.color = "blue";
+    nav2.style.color = "black";
+    nav3.style.color = "black";
   }else if(index==1){
     listtitle.classList.add("active")
     shelfbook.classList.add("active")
     contact.classList.remove("active")
     userinput.classList.add("active")
+    nav1.style.color = "black";
+    nav2.style.color = "blue";
+    nav3.style.color = "black";
   }else if(index==2){
     listtitle.classList.add("active")
     shelfbook.classList.add("active")
     contact.classList.add("active")
     userinput.classList.remove("active")
+    nav1.style.color = "black";
+    nav2.style.color = "black";
+    nav3.style.color = "blue";
   }
 }
 )})
